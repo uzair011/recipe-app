84c9792e09c785f9b8d6cc7e442d8f8d762a47b5
uzair011 <8407uzair@gmail.com>
2022-09-19 21:57:33 +0530

    Second Commit- navigation

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,11 +1,18 @@
-import { View, Pressable, Text } from "react-native"
+import { View, Pressable, Text, StyleSheet,Platform } from "react-native"
 
-function CategoryGridTile({title, color}) {
+function CategoryGridTile({ title, color, onPress }) {
     return (
-        <View>
-            <Pressable>
-                <View>
-                    <Text>{title }</Text>
+        <View style={styles.gridItem} >
+            <Pressable
+                android_ripple={{ color: "#ccc" }}
+                style={({ pressed }) =>
+                    [styles.pressableButton,
+                        pressed ? styles.buttonPressed : null
+                    ]}
+                onPress={onPress}
+            >
+                <View style={[styles.innerContainer, { backgroundColor: color }]} >
+                    <Text style={styles.title}>{title}</Text>
                 </View>
             </Pressable>
         </View>
@@ -13,3 +20,38 @@ function CategoryGridTile({title, color}) {
 }
 
 export default CategoryGridTile
+
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        margin: 16,
+        height: 150,
+        borderRadius: 8,
+        elevation: 4,
+     
+        backgroundColor: "white",
+        shadowColor: "black",
+        shadowOpacity: 0.25,
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 8,
+
+        overflow: Platform.OS === "ios"? "visible" : "hidden"
+    },
+    pressableButton: {
+        flex: 1
+    },
+    buttonPressed: {
+        opacity: 0.5
+    },
+    innerContainer: {
+        flex: 1,
+        padding: 15,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 8
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 18
+    }
+})
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,23 +2,30 @@ import { View, FlatList } from "react-native";
 import CategoryGridTile from "../components/CategoryGridTile";
 import { CATEGORIES } from "../data/dummy-data";
 
+function CategoriesScreen({navigation}) {
 function renderCategoryItemHandler(itemData) {
+
+    function pressHandler() {
+        navigation.navigate("MealsOverview")
+    }
+
     return (
         <CategoryGridTile
             title={itemData.item.title}
             color={itemData.item.color}
+            onPress={pressHandler}
         />
     )
 }
 
-function CategoriesScreen() {
     return (
         <FlatList
             data={CATEGORIES}
             keyExtractor={(item) => item.id}
             renderItem={renderCategoryItemHandler}
+            numColumns={2}
         />
     )
 }
 
-export default CategoriesScreen
+export default CategoriesScreen  
