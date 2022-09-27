4a2e8a58dbc51419cdde0f8fc4e523bb401240dc
uzair011 <8407uzair@gmail.com>
2022-09-27 22:48:08 +0530

    Seventh Commit- nested navigations

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import FavouritesScreen from "./screens/FavouritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Categories" component={CategoriesScreen} />
+      <Drawer.Screen name="Favourites" component={FavouritesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -22,33 +35,16 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
-            options={{
-              title: "All Categories",
-            }}
+            name="DrawerScreen"
+            component={DrawerNavigator}
+            options={
+              {
+                //title: "All Categories",
+              }
+            }
           />
-          <Stack.Screen
-            name="MealsOverview"
-            component={MealsOverviewScreen}
-            //   options={({ route, navigation }) => {
-            //   const category_id = route.params.categoryId
-
-            //   return {
-            //     title: category_id
-            //   }
-            // } }
-          />
-          <Stack.Screen
-            name="MealDetails"
-            component={MealDetailScreen}
-            options={{
-              title: "Meal Details",
-              // headerRight: () => {
-              //   return <Button title={"Add to Favourite"} />;
-              // },
-            }}
-          />
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+          <Stack.Screen name="MealDetails" component={MealDetailScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
---
+++ screens/FavouritesScreen.js
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function FavouritesScreen() {
+  return (
+    <View>
+      <Text>this is FavouritesScreen</Text>
+    </View>
+  );
+}
+
+export default FavouritesScreen;
